4c2189215ec63a773e38f9bee4eba79be8d3af8c
Martin Grunbaum <martin@itsolveonline.net>
2015-02-14 15:45:03 +0100

    Player movement
    - Basic player movement implemented using Rigidbody2D.

---
+++ Assets/Scripts/Input/KeyboardInput.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardInput : MonoBehaviour {
+    [SerializeField]
+    private MovementController movementController;
+
+    private bool leftPressed = false;
+    private bool rightPressed = false;
+    private bool upPressed = false;
+    private bool downPressed = false;
+
+    void FixedUpdate()
+    {
+        Vector2 movement = new Vector2();
+        if (leftPressed) movement += new Vector2(-1, 0);
+        if (rightPressed) movement += new Vector2(1, 0);
+        if (upPressed) movement += new Vector2(0, 1);
+        if (downPressed) movement += new Vector2(0, -1);
+
+        if (movement != Vector2.zero) movementController.Move(movement);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) upPressed = true;
+        else upPressed = false;
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) downPressed = true;
+        else downPressed = false;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) leftPressed = true;
+        else leftPressed = false;
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) rightPressed = true;
+        else rightPressed = false;
+    }
+}
---
+++ Assets/Scripts/Player/MovementController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementController : MonoBehaviour {
+    private Rigidbody2D rb2d;
+    
+    [SerializeField]
+    private float _baseMovement = 100f;
+
+    void Awake()
+    {
+        this.rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(Vector2 moveDir)
+    {
+        var final = new Vector2(moveDir.x * _baseMovement * Time.fixedDeltaTime, moveDir.y * _baseMovement * Time.fixedDeltaTime);
+        this.rb2d.velocity = final;
+    }
+}
