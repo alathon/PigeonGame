d654755220556a0b89949409715384dcedc0d5a5
Martin J <martinnjorgensen@gmail.com>
2015-02-14 22:52:57 +0100

    Begun work on movement system for AI.

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,54 +1,33 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
+﻿using Assets.Scripts.Weapons;
+using UnityEngine;
 
-/**
- * Enum to define instruction type, currently moving, shooting and move-while-shooting are supported.
- */
-public enum InstructionType
+namespace Assets.Scripts.Enemy
 {
-    Move,
-    Shoot,
-    MoveAndShoot
-}
-
-/**
- * Parent class for all instructions, must define a type to allow later parsing to identify them.
- * These classes are meant as containers, and should preferable not contain executable code, just variables.
- */
-public abstract class Instruction
-{
-   public new abstract InstructionType GetType();
-}
-
-/**
- * Implementation of the Move instruction.
- */
-public class MoveInstruction : Instruction
-{
-    public Vector2 Destination;
-
-
-    public override InstructionType GetType()
+    /// <summary>
+    /// Enemy unit/AI.
+    /// </summary>
+    public class Enemy : MonoBehaviour
     {
-        return InstructionType.Move;
+        // Basic stats
+        public Vector2 Position;     // Enemies position on screen.
+        public float BaseSpeed = 5f; // Enemies base movement speed.
+        public int Healthpoints = 100;
+
+        // Navigation/AI related stuff.
+        public Instruction[] Instructions;                 // List of Instructions the AI must carry out.
+        public bool RepeatInstructionsOnCompletion = true; // Should the enemy reåeat the instruction list if it completes all?
+
+        // Weapon related stuff.
+        public WeaponSystem Weapons; // List of weapons the enemy carries.
+
+        // Use this for initialization
+        void Start () {
+
+        }
+	
+        // Update is called once per physics frame
+        void FixedUpdate () {
+	
+        }
     }
-}
-
-public class Enemy : MonoBehaviour
-{
-
-    public Vector2 Position;     // Enemies position on screen.
-    public float BaseSpeed = 5f; // Enemies base movement speed.
-    public WeaponSystem Weapons; // List of weapons the enemy carries.
-
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 }
---
+++ Assets/Scripts/Enemy/Instruction.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Enemy
+{
+    /// <summary>
+    /// Enum to define instruction type, currently moving, shooting and move-while-shooting are supported.
+    /// </summary>
+    public enum InstructionType
+    {
+        Move,
+        Shoot,
+        MoveAndShoot
+    }
+
+    /// <summary>
+    /// Basic Instruction class that every instruction implementation must follow.
+    /// </summary>
+    public abstract class Instruction : MonoBehaviour
+    {
+        public new abstract InstructionType GetType();
+    }
+
+    /// <summary>
+    /// Basic movement instruction, allows for setting a number of waypoints to follow.
+    /// </summary>
+    public class MoveInstruction : Instruction
+    {
+        public Vector2[] Waypoints;
+
+        public override InstructionType GetType()
+        {
+            return InstructionType.Move;
+        }
+    }
+}
