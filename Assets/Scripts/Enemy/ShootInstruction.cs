81db21bbc6e04708628efba297d5d1a288e4e7fc
Martin J <martinnjorgensen@gmail.com>
2015-02-15 15:40:53 +0100

    Shoot instruction added.

--- Assets/Scripts/Enemy/ShootInstruction.cs
+++ Assets/Scripts/Enemy/ShootInstruction.cs
@@ -3,15 +3,35 @@
 namespace Assets.Scripts.Enemy
 {
     /// <summary>
-    /// Basic movement instruction, allows for setting a number of waypoints to follow.
+    /// Basic shoot instruction, sets a duration to keep weapons on for.
     /// </summary>
     public class ShootInstruction : Instruction
     {
         public float Duration = 4f; // How many seconds to shoot for.
+        private float _timeRemaining = -1; // How many seconds do we need to shoot before completion?
 
         public override InstructionType GetType()
         {
             return InstructionType.Shoot;
+        }
+
+        void Awake()
+        {
+            _timeRemaining = Duration;
+        }
+
+        /// <summary>
+        /// Signal that there have been shooting.
+        /// The instruction gets reset if there is no more time left.
+        /// </summary>
+        /// <param name="seconds">The amount of time expended.</param>
+        /// <returns>True if there is not more time left. False if there is more firing time left.</returns>
+        public bool ShootFor(float seconds)
+        {
+            _timeRemaining -= seconds;
+            if (_timeRemaining > 0) return false;
+            _timeRemaining = Duration;
+            return true;
         }
     }
 }
