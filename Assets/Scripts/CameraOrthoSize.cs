97139a7265fcf310b582546ed78da3245e9951a1
Martin Grunbaum <martin@itsolveonline.net>
2015-02-15 16:15:45 +0100

    Camera orthographic size.

---
+++ Assets/Scripts/CameraOrthoSize.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraOrthoSize : MonoBehaviour {
+
+	// Use this for initialization
+    void Update()
+    {
+        Debug.Log(Screen.height);
+        const float UnitsPerPixel = 1f/100f;
+        Camera.main.orthographicSize = Screen.height / 2f * UnitsPerPixel;
+    }
+}
